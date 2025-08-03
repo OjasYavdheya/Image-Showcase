71aad51d579a3619708f4be8a389e272a699baf7
Harsh <doingonlygoodthings@gmail.com>
2025-08-03 05:40:12 +0000

    alpha build complete

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function openModal(img) {
   const modal = document.getElementById("modal");
   const modalImg = document.getElementById("modalImg");
+  const modalPrompt = document.getElementById("modalPrompt");
+
   modalImg.src = img.src;
+  modalPrompt.textContent = "Prompt: " + img.dataset.fullprompt;
+
   modal.style.display = "flex";
 }
 
